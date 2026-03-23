e6ea540e091ab34e4e63f95289a98f19a8505d37
Mikuedgy <villalobosantiagou@gmail.com>
2026-03-23 13:28:05 -0500

    lista simple sin cola algunos metodos

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -7,16 +7,69 @@ public class SinglyLinkedList {
         size = 0;
     }
 
-    public void pushFront(int data) {}
-    public void pushBack(int data) {}
-    public int popFront() { return -1; }
-    public int popBack() { return -1; }
-    public Node find(int data) { return null; }
-    public void erase(Node node) {}
-    public void addBefore(Node node, int data) {}
-    public void addAfter(Node node, int data) {}
-    public boolean isEmpty() { return head == null; }
+    public void pushFront(int data) {
+        Node nuevo = new Node (data);
+        if (isEmpty ()){
+            head = nuevo;
+        }else{
+            nuevo.next = head;
+            head = nuevo;
+        }
+            size++;
+    }
+
+    public void pushBack(int data) {
+
+    }
+
+    public int popFront() {
+        if (isEmpty ()) return -1;
+
+        Node out = head;
+        head = head.next;
+        out.next = null;
+
+        size--;
+        return out.data; 
+    }
+
+    public int popBack() {
+        if(isEmpty())return -1;
+        
+        Nodo current = head;
+        
+        while(current.next.next != null){
+            current = current.next;
+        }
+        
+        int data = current.next.data;
+        
+        current.next = null
+        size--;
+        return data; 
+    }
+
+    public Node find(int data) { 
+        return null; 
+    }
+
+    public void erase(Node node) {
+    }
+
+    public void addBefore(Node node, int data) {
+
+    }
+
+    public void addAfter(Node node, int data) {
+    }
+
+    public boolean isEmpty() { 
+        return head == null; 
+    }
     public int topFront() { return head != null ? head.data : -1; }
     public int topBack() { return -1; }
-    public int size() { return size; }
+
+    public int size() {
+        return size; 
+    }
 }
